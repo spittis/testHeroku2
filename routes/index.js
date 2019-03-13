3c4bb373e42fc278e9357a9ce24ff4186e68a049
Soraya Pittis <31996896+spittis@users.noreply.github.com>
2019-03-13 13:51:10 -0400

    heellooo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const data = require(`../data`);
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log('data:', data);
+  console.log('user', rep.params.user);
+  res.render('userpage', data[req.params.user]); //what we want to render
 });
 
 module.exports = router;
