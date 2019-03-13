8fd8b58ee04a065947e863c1a2fb3b860fcc8ad9
Soraya Pittis <31996896+spittis@users.noreply.github.com>
2019-03-13 14:14:46 -0400

    hi

--- app.js
+++ app.js
@@ -19,29 +19,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const port = process.env.PORT || 3000;
-
+// const port = process.env.PORT || 3000; //make sure to get the capitalization and lowercases right 
+//this is where express stores web files => css, javascript, images, etc
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 //module.exports = app;
 
 app.listen(port, ()=> {
-  console.log(`app is running @ ${port}`);
+  console.log(`app is running @ ${port}`);
 })
