aff82d8c9fc976077b7a303145a19f7e99be2556
Soraya Pittis <31996896+spittis@users.noreply.github.com>
2019-03-13 13:58:32 -0400

    starting over lol

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const data = require(`../data`);
-
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('data:', data);
-  console.log('user', rep.params.user);
-  res.render('userpage', data[req.params.user]); //what we want to render
+  res.render('index', { title: 'Express', message: "Welcome to your first Express app" });
 });
 
 module.exports = router;
